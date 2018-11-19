6e82b51234ca5820103579fe00dc5c3a24dd43af
callin <callin2@gmail.com>
2018-11-19 11:22:03 +0900

    documentation

---
+++ agensgraph/build.sh
@@ -0,0 +1,2 @@
+docker build -t callin2/agens_http:latest .
+docker image tag callin2/agens_http:latest callin2/agens_http:1.4.0
--- agensgraph/docker-entrypoint.sh
+++ agensgraph/docker-entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 ag_ctl start
 sleep 3
 
+cp /home/agens/Agens_Http/api_server/dbinfo.js.sample /home/agens/Agens_Http/api_server/dbinfo.js
+npm start --prefix /home/agens/Agens_Http/api_server
+
 exec "$@"
